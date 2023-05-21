20845e2d3bda242081e675727ccb5ad873d9b179
IO3683 <nu-dopustim@mail.ru>
2023-05-21 16:05:51 +0600

    first commit

--- calc.py
+++ calc.py
@@ -2,25 +2,29 @@ from parser import parse
 
 
 def calc(currency, value, desired):
-    if (isinstance(value, int) == False) and (isinstance(value, float) == False):
-        return "TypeError"
+    try:
+        value = float(value)
+    except ValueError:
+        return 'ValueError: "' + value + '"' + ' - не является числом'
+    except TypeError:
+        return 'TypeError: недопустимое значение'
     if value < 0:
-        return "ValueError"
+        return 'ValueError: (' + str(value) + ') меньше нуля'
     if currency == "RUB":
         if desired == currency:
             return value
         else:
             new = parse(desired)
             result = float(value) / new
-            return float('{:.4f}'.format(result))
+            return "The answer is " + '{:.4f}'.format(result)
     else:
         if desired == "RUB":
             result = float(value) * parse(currency)
-            return float('{:.4f}'.format(result))
+            return "The answer is " + '{:.4f}'.format(result)
         if desired == currency:
             return value
         else:
             val1 = parse(desired)
             val2 = parse(currency)
             result = (float(val2) / float(val1)) * float(value)
-            return float('{:.4f}'.format(result))
+            return "The answer is " + '{:.4f}'.format(result)
--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def form():
         currency = request.form.get('list')
         value = request.form.get('value')
         desired = request.form.get('desired')
-        print(value)
         return render_template('index.html', ans=calc(currency, value, desired))
     else:
         return render_template('index.html')
@@ -19,15 +18,16 @@ def form():
 
 class TestConverter(unittest.TestCase):
     def test_values(self):
-        self.assertEqual(calc("USD", 16, "RUB"), 1279.6768)
-        self.assertEqual(calc("USD", 16.55, "RUB"), 1323.6657)
-        self.assertEqual(calc("USD", -13, "RUB"), "ValueError")
+        self.assertEqual(calc("USD", 16, "RUB"), "The answer is 1278.5488")
+        self.assertEqual(calc("USD", 16.55, "RUB"), "The answer is 1322.4989")
+        self.assertEqual(calc("USD", -13, "RUB"), "ValueError: (-13.0) меньше нуля")
 
     def test_types(self):
-        self.assertEqual(calc("USD", "16", "RUB"), "TypeError")
-        self.assertEqual(calc("USD", [1, 2, 3], "RUB"), "TypeError")
-        self.assertEqual(calc("USD", (1, 2, 3), "RUB"), "TypeError")
+        self.assertEqual(calc("USD", "шестнадцать", "RUB"), 'ValueError: "шестнадцать" - не является числом')
+        self.assertEqual(calc("USD", [1, 2, 3], "RUB"), 'TypeError: недопустимое значение')
+        self.assertEqual(calc("USD", (1, 2, 3), "RUB"), 'TypeError: недопустимое значение')
+
 
 if __name__ == "__main__":
-    #app.run(debug=True)
-    unittest.main()
+    app.run(debug=True)
+unittest.main()
